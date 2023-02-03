a20421016f5bddd0c0223a7b08c6185d1417e1c6
zidankhan86 <zidankhan718@gmail.com>
2023-02-03 20:01:25 +0600

    Swith,Case

---
+++ swithcase.php
@@ -0,0 +1,33 @@
+<?php
+
+$number=90;
+$number=80;
+$number=60;
+switch ($number) {
+
+case 60:
+    echo ("Failed");
+    break;
+case 80:
+    echo ("D+");
+    break;
+case 90:
+    echo ("B+");
+    break;
+case 4:
+    echo ("A");
+    break;
+    
+    default:
+    echo ("MY NUMBER IS 90");
+    break;
+
+
+
+    
+
+}
+
+
+
+?>
