c17bb05ef9ec28c0a4245af601bc65cc499948cd
zidankhan86 <zidankhan718@gmail.com>
2023-02-04 23:45:17 +0600

    Constant

---
+++ vardamp.php
@@ -0,0 +1,14 @@
+<?php
+/*echo "Hello Bangladesh\n";
+echo 1971;
+$name="Earth";
+$age=1971;
+$x='Bangladesh';*/
+$result=false;
+$anotherResult=true;
+$stringResult="True";
+/*Var_dup use korle mara bujte pari j konta kon dhoroner Datatype.
+Ekhane 3 ta variable diye 3 dhoroner data Run korano hoise.*/
+var_dump($stringResult);
+var_dump($anotherResult);
+var_dump($stringResult);
