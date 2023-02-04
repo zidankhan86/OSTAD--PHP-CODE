0cbae4cead6b0dd8614e0d7970b73b3eab50f205
zidankhan86 <zidankhan718@gmail.com>
2023-02-04 23:20:05 +0600

    PI

--- constant.php
+++ constant.php
@@ -5,4 +5,5 @@ define('PI',14.1001);
 //echo constant ('PI');
 //$constant='constant';
 //echo "value of paie ={$constant('PI')}";
-echo "The value of PI=".PI;
+//echo "The value of PI=".PI;
+echo PI;
