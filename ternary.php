8b98c7929a5e33ee72516a5f11afc508876ae735
zidankhan86 <zidankhan718@gmail.com>
2023-02-05 11:46:26 +0600

    sigle lin ternary

---
+++ ternary.php
@@ -0,0 +1,12 @@
+
+        
+
+     <?php
+    $x=100;
+     $output=($x>=100)?($x."is greater than and equal to 100"):($x."is less than or equal 100");
+     echo $output;
+
+    /* $x=19;
+     $y=($x>=20)?($x." 19 is True"):("False") ;
+     echo $y;*/
+     
