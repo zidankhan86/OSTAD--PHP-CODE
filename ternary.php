c8a5b93a9e06d8b28b2ac13b954a21c29172184b
zidankhan86 <zidankhan718@gmail.com>
2023-02-05 14:14:05 +0600

    Update

--- ternary.php
+++ ternary.php
@@ -3,10 +3,11 @@
 
      <?php
     $x=100;
-     $output=($x>=100)?($x."is greater than and equal to 100"):($x."is less than or equal 100");
+    // $output=($x>=100)?($x."is greater than and equal to 100"):($x."is less than or equal 100");
      echo $output;
 
     /* $x=19;
      $y=($x>=20)?($x." 19 is True"):("False") ;
      echo $y;*/
-     
+     $output=($x>=50)?("This equal or may 50"):("False");
+     echo $output;
