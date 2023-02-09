727fd6486554452b3c3663495bc074cecf986b96
zidankhan86 <zidankhan718@gmail.com>
2023-02-09 14:06:25 +0600

    Ternary

--- binaryOctalDecimalHexadecimal.php
+++ binaryOctalDecimalHexadecimal.php
@@ -1,7 +1,7 @@
 <?php
 $b=12;
 $d=012;
-$h=017;
+$h=1;
 $o=05;
 
 printf("This is %s, %s, %s ,%s",$b,$d,$h,$o);
--- ternary.php
+++ ternary.php
@@ -2,12 +2,19 @@
         
 
      <?php
-    $x=100;
+    $x=1000;
+    //$x=1000;
     // $output=($x>=100)?($x."is greater than and equal to 100"):($x."is less than or equal 100");
-     echo $output;
+     //echo $output;
 
     /* $x=19;
      $y=($x>=20)?($x." 19 is True"):("False") ;
      echo $y;*/
-     $output=($x>=50)?("This equal or may 50"):("False");
-     echo $output;
+     //$output=($x>=50)?(($x<=100)?("True"):("False")):("Not Matched");
+     
+
+    // $output=($x=40)?(($x=>4)?(()?(()?():()):()):()):();
+
+     //$output=($x>=40)?(($x>=45)?(($x>=50)?("{$x} is grater than or equal 50"):("{$x} less than 50")):("{$x} is grater than 45")):("{$x} is less than 40");
+     $output=($x>=500)?(($x>=1000)?("{$x} is greater than or equal 1000"):("{$x}is less than 100")):("Less than 40");
+     printf($output) ;
