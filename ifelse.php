926d5da9059edb324dd2d3425bc2f72fada4da5e
zidankhan86 <zidankhan718@gmail.com>
2023-02-10 11:56:11 +0600

    ifelse

---
+++ ifelse.php
@@ -0,0 +1,21 @@
+<?php
+$number=12;
+$number2=13;
+
+//if($number%2) {
+
+    //printf("Even");
+
+//}else{
+
+   // printf("odd");
+//}
+
+if ($number!=$number2){
+
+    printf("Not Equal");
+}
+else{
+
+    printf("Equal to");
+}
