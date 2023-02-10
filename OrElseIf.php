6339da9c70436184b0e4059f609fb5d6efdcc22f
zidankhan86 <zidankhan718@gmail.com>
2023-02-10 13:08:14 +0600

    AND OR operator

---
+++ OrElseIf.php
@@ -0,0 +1,12 @@
+<?php
+$food="banana";
+
+if($food=="apple"||$food="banana"){
+
+    printf("Its a Fruit");
+}else{
+
+printf("Soory! There is no fruit");
+
+}
+
---
+++ ifelseLogicaloperator.php
@@ -0,0 +1,12 @@
+<?php
+$age=15;
+
+if($age>=13 && $age<=19){
+
+
+    printf("He or she is teen");
+}
+else{
+
+    printf("She or He is Kids");
+}
