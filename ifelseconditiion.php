364e97808a6c9663eed83d2001fea7b5266868cd
zidankhan86 <zidankhan718@gmail.com>
2023-02-10 20:40:59 +0600

    True AND OR False Condition

---
+++ ifelseconditiion.php
@@ -0,0 +1,14 @@
+<?php
+
+$dipty=55;
+$y=6;
+
+if($dipty <=    6 )
+{
+
+printf("I am less than or equal to 6");
+
+}else{
+
+    printf("I am greater than 6");
+}
---
+++ trueFalseLogic.php
@@ -0,0 +1,24 @@
+<?php
+
+//$number=200;
+
+/*if(true || false)//Only false or false then will be false Except all are True
+if(true && true)// Only when true && true will be true Except all are false
+
+
+{
+
+printf("Hi i am true");
+}*/
+
+
+if(false || false){
+
+    printf("Wow I got Burger");
+}
+else{
+
+    printf("Sorry! No Burger Found");
+}
+
+
