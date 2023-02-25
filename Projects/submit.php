081fcb15c5172cf51fbff96cb0ab601225614ca0
zidankhan86 <zidankhan718@gmail.com>
2023-02-25 13:54:50 +0600

    GET and REQUEST

--- Projects/submit.php
+++ Projects/submit.php
@@ -21,7 +21,26 @@ echo "<p>$message</p>";
 }else{
     echo "<p> Error! This is a Invalid Action</p>";
 }
+// Output query string parameters using $_GET
 
+if(!empty($_GET)){
+echo "<h1>This is for Query String Parameters:</h1>";
+echo "<ul>";
+foreach($_GET as $key => $value){
+    echo "<li> $key : $value</li>";
+}
+echo "</ul>";
+
+}
+ // Output all request parameters using $_REQUEST
+ if (!empty($_REQUEST)) {
+    echo "<h2>All Request Parameters:</h2>";
+    echo "<ul>";
+    foreach ($_REQUEST as $key => $value) {
+      echo "<li>$key: $value</li>";
+    }
+    echo "</ul>";
+  }
 
     ?>
 
