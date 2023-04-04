72641f7cd58eb03ff92c77edd74255178f438479
zidankhan86 <zidankhan718@gmail.com>
2023-04-04 15:12:15 +0600

    Ostad Assignment 9

---
+++ assignmentcss/contact.php
@@ -0,0 +1,18 @@
+<?php
+if (isset($_POST['submit'])) {
+    // Retrieve form data
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    // Validate form data
+    if (empty($name) || empty($email) || empty($subject) || empty($message)) {
+        $error_message = "All fields are required.";
+    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $error_message = "Invalid email address.";
+    } else {
+        // Display success message
+        $success_message = "Your message has been sent successfully.";
+    }
+}
